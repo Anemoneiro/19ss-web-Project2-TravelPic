dceb94bdb283b3a61090c1afa4407a45389e49da
Anemoneiro <hirotostarline@sina.com>
2020-06-21 22:49:43 +0800

    pj2 v1.3:fix modify without description bug

--- src/php/upload.php
+++ src/php/upload.php
@@ -93,7 +93,7 @@ if (isset($_SESSION['Username'])) {
                 } else if ($cityCode == 'null') {
                     $sql_modify = "UPDATE travelimage SET PATH='$picPath', Title='$title', Description='$description', CityCode=NULL, CountryCodeISO='$countryCodeISO', Content='$content' WHERE ImageID='$imageID'";
                 } else {
-                    "UPDATE travelimage SET PATH='$picPath', Title='$title', Description=NULL, CityCode='$cityCode', CountryCodeISO='$countryCodeISO', Content='$content' WHERE ImageID='$imageID'";
+                    $sql_modify = "UPDATE travelimage SET PATH='$picPath', Title='$title', Description=NULL, CityCode='$cityCode', CountryCodeISO='$countryCodeISO', Content='$content' WHERE ImageID='$imageID'";
                 }
 
                 $result_modify = $pdo->query($sql_modify);
